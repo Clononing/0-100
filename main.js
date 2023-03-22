b54953490e4da0d7d06afacdb564e9780a9f11b7
DacostaAdrian <128481962+DacostaAdrian@users.noreply.github.com>
2023-03-22 08:41:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+var num = Math.floor(Math.random(0,100)*100)
+var contador = 0 
 
+var num2 = prompt(parseInt("Introduzca un número"))
+
+while (num!=num2 && contador<10){
+    
+    if (num2<num){
+        document.write("El número que buscas es mayor")
+        contador = contador + 1
+    }
+    
+    if (num2>num){
+        document.write("El número que buscas es menor")
+        contador = contador + 1
+    }
+    
+    if (num2=num){
+        document.write("El número que buscas es correcto")
+        contador = contador + 1 
+    }
+    
+    var num2 = prompt(parseInt("Introduzca un número"))
+
+    
+}
+
+if (contador = 10){
+    document.write("El numero correcto es", num)
+}
